d9d8fb5e8522be48f91ade5590b4945a3b7d4123
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-05-01 14:19:50 -0600

    whatever

---
+++ source/command-handler/server-commands/asa-server-restart.ts
@@ -0,0 +1,54 @@
+import type { ServiceResponse, PlayerResponse } from '../../modules/interfaces';
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import type { SlashCommandProps, CommandOptions } from 'commandkit';
+import { nitrado } from '../../other/config.json';
+import axios, { AxiosResponse } from 'axios';
+
+export const data = new SlashCommandBuilder()
+  .setName('asa-gameserver-restart')
+  .setDescription('Performs an in-game server action.')
+  .addStringOption(option => option.setName('identifier').setDescription('Selected action will be performed on given server.').setRequired(true))
+
+export async function run({ interaction, client, handler }: SlashCommandProps) {
+  await interaction.deferReply({ ephemeral: false });
+  const start: number = performance.now();
+
+  const platforms: string[] = ['arksa'];
+
+  interface InteractionInput {
+    identifier: string | null;
+    admin: string;
+  };
+
+  interface GameserverRestart {
+    status: string;
+    message: string;
+  };
+
+  const input: InteractionInput = {
+    identifier: interaction.options.getString('identifier'),
+    admin: interaction.user.id
+  };
+
+  const gameserver = async () => {
+    try {
+      const url: string = `https://api.nitrado.net/services/${input.identifier}/gameservers/restart`;
+      const response: AxiosResponse<GameserverRestart> = await axios.post(url, { identifier: input.identifier }, { headers: { 'Authorization': nitrado.token } });
+      if (response.status !== 200) { return console.log('Invalid') };
+    } catch (error: any) {
+      console.log(error.response)
+    };
+  };
+
+  const service = async () => {
+    const url: string = 'https://api.nitrado.net/services';
+    const response: AxiosResponse<ServiceResponse> = await axios.get(url, { headers: { 'Authorization': nitrado.token } });
+    if (response.status === 200) { await gameserver() };
+  };
+
+  await service();
+};
+
+export const options: CommandOptions = {
+  userPermissions: ['Administrator'],
+};
